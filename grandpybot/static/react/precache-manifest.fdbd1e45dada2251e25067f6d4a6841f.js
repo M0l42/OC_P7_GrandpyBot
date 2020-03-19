1f678217506f93c494cfb9f6a8b3e43fc84d8065
root <boukobza.nathan@gmail.com>
2020-03-19 14:55:35 +0100

    add settings and static

---
+++ grandpybot/static/react/precache-manifest.fdbd1e45dada2251e25067f6d4a6841f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597c8db1e4ecae2609b47c8528842919",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "2a263ffc0e902550012c",
+    "url": "/static/react/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "45418b72b81e9f12473b",
+    "url": "/static/react/css/main.3a794063.chunk.css"
+  },
+  {
+    "revision": "2a263ffc0e902550012c",
+    "url": "/static/react/js/2.0d6b0b57.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/react/js/2.0d6b0b57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45418b72b81e9f12473b",
+    "url": "/static/react/js/main.ab7f19a9.chunk.js"
+  },
+  {
+    "revision": "d596ff4eb241b77924f5",
+    "url": "/static/react/js/runtime-main.2298e5f2.js"
+  }
+]);
