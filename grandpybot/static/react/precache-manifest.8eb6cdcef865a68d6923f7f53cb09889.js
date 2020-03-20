5f5b35039a1162b7a98159bae373b6b039285811
root <boukobza.nathan@gmail.com>
2020-03-20 19:55:55 +0100

    make the app more responsive

---
+++ grandpybot/static/react/precache-manifest.8eb6cdcef865a68d6923f7f53cb09889.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93ce1301d634da4fb1ba376133a881d",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "62b3c23bef00951dc6c5",
+    "url": "/static/react/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b031a4ed6a06547e9819",
+    "url": "/static/react/css/main.e813d489.chunk.css"
+  },
+  {
+    "revision": "62b3c23bef00951dc6c5",
+    "url": "/static/react/js/2.46a840ec.chunk.js"
+  },
+  {
+    "revision": "8ce2385a9ea7439468621fb86ef6a20c",
+    "url": "/static/react/js/2.46a840ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b031a4ed6a06547e9819",
+    "url": "/static/react/js/main.528c48f4.chunk.js"
+  },
+  {
+    "revision": "d596ff4eb241b77924f5",
+    "url": "/static/react/js/runtime-main.2298e5f2.js"
+  }
+]);
