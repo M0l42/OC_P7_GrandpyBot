f44068f85a596fdb7fef14143580059f053e1fee
root <boukobza.nathan@gmail.com>
2020-03-20 20:37:07 +0100

    delete w-75 and add a new css media

---
+++ grandpybot/static/react/precache-manifest.19c3501a9ef2ea2d387a83516a999351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07561726536b5b3b2e7689c65b767dec",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "6dcd7fc27b183e637df5",
+    "url": "/static/react/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f5c5027a8e233c668ef5",
+    "url": "/static/react/css/main.9f1b54ec.chunk.css"
+  },
+  {
+    "revision": "6dcd7fc27b183e637df5",
+    "url": "/static/react/js/2.57e46314.chunk.js"
+  },
+  {
+    "revision": "8ce2385a9ea7439468621fb86ef6a20c",
+    "url": "/static/react/js/2.57e46314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c5027a8e233c668ef5",
+    "url": "/static/react/js/main.82d3ae28.chunk.js"
+  },
+  {
+    "revision": "d596ff4eb241b77924f5",
+    "url": "/static/react/js/runtime-main.2298e5f2.js"
+  }
+]);
