780c27254c34029419a955978d1ff57e951ff641
root <boukobza.nathan@gmail.com>
2020-03-21 04:23:02 +0100

    front fix + test for new function

---
+++ grandpybot/static/react/precache-manifest.4072d9dbb8032807a4fc1a434ff466cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ef36c337cc6c0703fa116b98ddfc3b",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "04a8b235fcc2a0acfc5c",
+    "url": "/static/react/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b33ceadd4d319166845a",
+    "url": "/static/react/css/main.3d3d9ffe.chunk.css"
+  },
+  {
+    "revision": "04a8b235fcc2a0acfc5c",
+    "url": "/static/react/js/2.5fc05d9b.chunk.js"
+  },
+  {
+    "revision": "8ce2385a9ea7439468621fb86ef6a20c",
+    "url": "/static/react/js/2.5fc05d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33ceadd4d319166845a",
+    "url": "/static/react/js/main.2e290c48.chunk.js"
+  },
+  {
+    "revision": "d596ff4eb241b77924f5",
+    "url": "/static/react/js/runtime-main.2298e5f2.js"
+  }
+]);
